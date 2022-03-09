dc861d40bed54dbcd35e5f78eaca053854e4e30a
Dennywr <dennywahyudiramadhon@gmail.com>
2022-03-09 12:13:03 +0700

    Menambahkan file fungsi.php ke dalam folder Tugas2

---
+++ Tugas2/fungsi.php
@@ -0,0 +1,13 @@
+<?php 
+
+  function showName($name) {
+    return "Hello $name, my name is Dennywr";
+  }
+
+  echo showName("Michael");
+  echo "<br>";
+  echo showName("Lord Putin");
+  echo "<br>";
+  echo showName("Anya Geraldine");
+
+?>
