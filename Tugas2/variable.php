d1a2a5417930e4a61c548cc4b51b582928194379
Dennywr <dennywahyudiramadhon@gmail.com>
2022-03-09 11:59:45 +0700

    Menambahkan file variable.php ke dalam folder Tugas2

---
+++ Tugas2/variable.php
@@ -0,0 +1,20 @@
+<?php 
+
+  $num1 = 10;
+  $num2 = 5;
+  $num3 = 12;
+  $num4 = 8;
+
+  $penjumlahan = $num1 + $num4;
+  echo $penjumlahan;
+
+  $pengurangan = $num3 - $num2;
+  echo $pengurangan;
+
+  $perkalian = $num2 * $num4;
+  echo $perkalian;
+  
+  $pembagian = $num1 / $num2;
+  echo $pembagian;
+
+?>
